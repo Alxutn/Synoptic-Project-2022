1b655c62dd81e830154b28ed773a275b80c07e50
Alex <alexju809@gmail.com>
2022-06-01 15:39:22 +0100

    New Motion Sensor

--- src/MotionSensor.java
+++ src/MotionSensor.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
@@ -8,33 +9,56 @@ public class MotionSensor {
     float xCoord, yCoord;
     int previousSensor;
     Random rand = new Random();
-    LocalDateTime lastPing = 2007-12-03T10:15:30;
+    LocalDateTime lastPing = LocalDateTime.now();
 
     public MotionSensor(String inputLocation, float inputXCoord, float inputYCoord, int inputPreviousSensor){
         location = inputLocation;
         xCoord = inputXCoord;
         yCoord = inputYCoord;
         previousSensor = inputPreviousSensor;
+        checkSurroundings();
     }
 
     public boolean checkSurroundings(){
         //fake data about if an object is detected moving nearby
         boolean detected = false;
+
+        compareMonth(lastPing.getMonth());
+
         int n = rand.nextInt(100);
         if(n == 1) {
             detected = true;
         }
         if(detected){
             LocalDateTime timeDetectedRaw = LocalDateTime.now();
-            if(lastPing){
-                lastPing = timeDetectedRaw;
-            }else {
+
+            //if(lastPing){
+                //lastPing = timeDetectedRaw;
+            //}else {
+
                 DateTimeFormatter timeDetected = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
                 String formattedDate = timeDetectedRaw.format(timeDetected);
-            }
+            //}
             return true;
         }else{
             return false;
         }
     }
+
+    public void compareMonth(Month inputMonth){
+        if(inputMonth.equals(Month.JUNE)) {
+            System.out.println(inputMonth);
+        }
+    }
+    public void compareMinute(int inputMinute){
+        System.out.println(inputMinute);
+    }
+    public void compareYear(int inputYear){
+        System.out.println(inputYear);
+    }
+
+    public static void main(String[] args) {
+        MotionSensor sensor1 = new MotionSensor("RangerStation1", 0, 0, 0);
+    }
 }
+
