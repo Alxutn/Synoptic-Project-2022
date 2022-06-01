292c83b0ad48cd2d461a1d57ab023aecb26ded1a
Alex <alexju809@gmail.com>
2022-06-01 15:02:46 +0100

    Initial Sensor Creation
    title

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+       MotionSensor sensor1 = new MotionSensor("RangerStation1", 0, 0, 0);
     }
 }
--- src/MotionSensor.java
+++ src/MotionSensor.java
@@ -1,3 +1,12 @@
 public class MotionSensor {
-    
+    String location;
+    float xCoord, yCoord;
+    int previousSensor;
+
+    public MotionSensor(String inputLocation, float inputXCoord, float inputYCoord, int inputPreviousSensor){
+        location = inputLocation;
+        xCoord = inputXCoord;
+        yCoord = inputYCoord;
+        previousSensor = inputPreviousSensor;
+    }
 }
