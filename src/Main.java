1b4b83429e2510eefacc93173c10090c961e2cc4
Alex <alexju809@gmail.com>
2022-06-01 16:38:28 +0100

    End of Lab1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-       MotionSensor sensor1 = new MotionSensor("RangerStation1", 0, 0, 0);
+       MotionSensor sensor1 = new MotionSensor("RangerStation1",1, 0, 0, 0);
     }
 }
--- src/MotionSensor.java
+++ src/MotionSensor.java
@@ -5,37 +5,76 @@ import java.util.Random;
 public class MotionSensor {
     String location;
     float xCoord, yCoord;
-    int previousSensor;
-    Random rand = new Random();
+    int previousSensor, sensorID;
     LocalDateTime lastPing = LocalDateTime.now();
+    //for random data
+    Random rand = new Random();
 
-    public MotionSensor(String inputLocation, float inputXCoord, float inputYCoord, int inputPreviousSensor){
+    public MotionSensor(String inputLocation, int inputSensorID, float inputXCoord, float inputYCoord, int inputPreviousSensor){
         location = inputLocation;
         xCoord = inputXCoord;
         yCoord = inputYCoord;
         previousSensor = inputPreviousSensor;
-        checkSurroundings();
+        sensorID = inputSensorID;
     }
 
-    public boolean checkSurroundings(){
+    public float getxCoord(){
+        return xCoord;
+    }
+    public float getyCoord(){
+        return yCoord;
+    }
+    public int getPreviousSensor(){
+        return previousSensor;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public int getSensorID(){
+        return sensorID;
+    }
+
+    public void setxCoord(float value){
+        xCoord = value;
+    }
+    public void setyCoord(float value){
+        yCoord = value;
+    }
+    public void setPreviousSensor(int value){
+        previousSensor = value;
+    }
+    public void setLocation(String value){
+        location = value;
+    }
+    public void setSensorID(int value){
+        sensorID = value;
+    }
+
+
+    public void checkSurroundings(){
         boolean detected = false;
+
         //fake data about if an object is detected moving nearby
         int n = rand.nextInt(100);
         if(n == 1) {
             detected = true;
         }
-        if(detected){
+
+        if(detected) {
             LocalDateTime timeDetectedRaw = LocalDateTime.now();
-            timeComparisons(timeDetectedRaw);
-            return true;
-        }else{
-            return false;
+            boolean newValue = timeComparisons(timeDetectedRaw);
+            if(newValue){
+                System.out.println("Movement Detected at: "+getLocation()+" at: "+lastPing);
+                //for next time start looking at CSV.
+            }
         }
     }
 
     public boolean timeComparisons(LocalDateTime inputTime) {
         //testAgainstTime = testAgainstTime.plusMinutes(5);
         LocalDateTime tempLastPing = lastPing.plusMinutes(5);
+        System.out.println(tempLastPing);
+        System.out.println(lastPing);
         if(inputTime.isAfter(tempLastPing)){
             lastPing = inputTime;
             //print it out somewhere!!
@@ -48,7 +87,7 @@ public class MotionSensor {
 
 
     public static void main(String[] args) {
-        MotionSensor sensor1 = new MotionSensor("RangerStation1", 0, 0, 0);
+        MotionSensor sensor1 = new MotionSensor("RangerStation1", 1, 0, 0, 0);
     }
 }
 
